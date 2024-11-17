e7c622a78194cc3e969322c68d493c72f9e5fba4
Vitor Markis <vitormarkis2369@gmail.com>
2024-11-17 07:56:44 -0300

    controlled inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,67 @@
-import { Reducer, useEffect, useReducer } from "react"
-import { toast } from "sonner"
-import { cn } from "./lib/utils"
-import { Button } from "./Button"
-
-type Action_LoggedIn = { type: "logout" }
-type Action_NoSession = { type: "login" }
-type Action = Action_LoggedIn | Action_NoSession
-
-type State = {
-  state: "logged-in" | "no-session"
-  handler: Record<string, Reducer<State, any>>
-}
-
-const handlers = {
-  "logged-in": {
-    logout: (prevState: State, action: Action_LoggedIn) => {
-      prevState.state = "no-session"
-      return prevState
-    },
-  },
-  "no-session": {
-    login: (prevState: State, action: Action_NoSession) => {
-      prevState.state = "logged-in"
-      return prevState
-    },
-  },
-} satisfies Record<State["state"], Record<string, Reducer<State, any>>>
-
-const reducer: Reducer<State, Action> = (currState, action) => {
-  let state = { ...currState }
-  if (!state.handler[action.type]) {
-    toast(`Unknown action [${action.type}]`)
-    return currState
-  }
-  state = state.handler[action.type]({ ...state }, action)
-
-  state.handler = handlers[state.state]
-  return state
-}
-
-const initialState: State = {
-  state: "no-session",
-  handler: handlers["no-session"],
-}
+import { useEffect, useState } from "react"
 
 function App() {
-  const [store, dispatch] = useReducer(reducer, initialState)
-
-  useEffect(() => {
-    console.log(store)
-    Object.assign(window, { store })
-  }, [store])
+  const [value, setValue] = useState(20)
 
   return (
     <>
-      {"logout" in store.handler && (
-        <Button onClick={() => dispatch({ type: "logout" })}>Logout</Button>
-      )}
-      {"login" in store.handler && (
-        <Button onClick={() => dispatch({ type: "login" })}>Login</Button>
-      )}
-      <p className={cn("", store.state !== "logged-in" && "opacity-30")}>Logged in</p>
-      <p className={cn("", store.state !== "no-session" && "opacity-30")}>No session</p>
+      <div className="bg-rose-200">
+        {value}
+        <button
+          onClick={() => {
+            setValue(0)
+          }}
+        >
+          Reset controlled to 0
+        </button>
+      </div>
+      <div className="p-2 bg-green-200">
+        <Timer
+          value={value}
+          onChange={setValue}
+        />
+      </div>
+    </>
+  )
+}
+
+type TimerProps = {
+  value?: number
+  onChange?: (value: number) => void
+}
+
+export function Timer({ onChange, ...props }: TimerProps) {
+  const value_ext = props.value
+  const [value_inn, setValue] = useState(value_ext ?? 0)
+  const value = "value" in props ? value_ext : value_inn
+  /**
+   * Se foi passado a prop value, sempre usa value externo,
+   * onChange vai setar o state do valor externo
+   *
+   * Ao mesmo tempo, mantém um estado interno para
+   * componente ser independente
+   *
+   * Usa o setState para setar o valor, mas o setState
+   * precisa ser custom, um wrapper em cima do setState
+   * do useState, que roda o onChange junto, assim garante
+   * que eles sempre estejam sincronizados
+   */
+
+  function increaseTimer() {
+    const finalValue = value + 1
+    setValue(finalValue)
+    onChange?.(finalValue)
+  }
+
+  useEffect(() => {
+    const timer = setInterval(increaseTimer, 1000)
+    return () => clearInterval(timer)
+  }, [value])
+
+  return (
+    <>
+      <strong>Timer: </strong>
+      <span>{value}</span>
     </>
   )
 }
