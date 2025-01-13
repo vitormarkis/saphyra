2a28d05566cfeff3c39c8d56f758f793590c063a
Vitor Markis <vitormarkis2369@gmail.com>
2025-01-13 20:14:29 -0300

    add analytics

--- src/providers.tsx
+++ src/providers.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { Toaster } from "sonner"
 import { GoToSourceButton } from "./go-to-source-button"
+import { Analytics } from "@vercel/analytics/react"
 
 type ProvidersProps = {
   children: React.ReactNode
@@ -19,6 +20,7 @@ export function Providers({ children }: ProvidersProps) {
       {children}
       <Toaster />
       <GoToSourceButton />
+      <Analytics />
     </>
   )
 }
