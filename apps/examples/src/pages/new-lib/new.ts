137267381195cbc9716011cea8503f65570d911c
Vitor Markis <vitormarkis2369@gmail.com>
2025-09-15 21:21:49 -0300

    builder

--- apps/examples/src/pages/new-lib/new.ts
+++ apps/examples/src/pages/new-lib/new.ts
@@ -1,4 +1,4 @@
-import { Subject } from "./impl"
+import { Subject, createSubject } from "./impl"
 
 type MOCK_STATE = { vitor: Subject<true>; age: Subject<20> }
 
@@ -13,19 +13,53 @@ type Reaction<TSelectors extends readonly ((state: MOCK_STATE) => any)[]> = {
   run: (...args: SelectorValues<TSelectors>) => void
 }
 
-function Selector<
-  const TSelectors extends readonly ((state: MOCK_STATE) => any)[],
->(
+function Selector<const TSelectors extends ((state: MOCK_STATE) => any)[]>(
   reaction: Reaction<TSelectors>
 ): [TSelectors, (...args: SelectorValues<TSelectors>) => void] {
   return [reaction.on, reaction.run]
 }
 
-Selector({
-  on: [s => s.vitor, s => s.age],
-  run: (vitor, age) => {
-    const vitor_value: true = vitor
-    const age_value: 20 = age
-    return `${vitor_value} ${age_value}`
-  },
-})
+function builder() {
+  let state: MOCK_STATE | undefined
+  let selectors: Array<Reaction<readonly ((state: MOCK_STATE) => any)[]>> = []
+
+  return {
+    state<TState extends MOCK_STATE>(newState: TState) {
+      state = newState
+      return {
+        selectors<const TSelectors extends ((state: MOCK_STATE) => any)[]>(
+          selectorList: Array<Reaction<TSelectors>>
+        ) {
+          selectors = selectorList
+          return {
+            build: () => ({
+              state,
+              selectors,
+            }),
+          }
+        },
+        build: () => ({
+          state,
+          selectors: [],
+        }),
+      }
+    },
+  }
+}
+
+const result = builder()
+  .state({
+    vitor: createSubject(true),
+    age: createSubject(20),
+  })
+  .selectors([
+    {
+      on: [s => s.vitor, s => s.age],
+      run: (vitor, age) => {
+        const vitor_value: true = vitor
+        const age_value: 20 = age
+        return `${vitor_value} ${age_value}`
+      },
+    },
+  ])
+  .build()
